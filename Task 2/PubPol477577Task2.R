5d921994ee9357017d678a5d807375581eefdd87
NakaFrozn <jonaszhxie@outlook.com>
2024-02-26 15:22:05 -0600

    Update 02262024

--- Task 2/PubPol477577Task2.R
+++ Task 2/PubPol477577Task2.R
@@ -26,15 +26,35 @@ df_bes_sav <- read_spss("./2010BESPreandPost.sav")
 str(df_bes_sav)
 
 ## Excel .xlsx
-### I have made some changes to the structure of the .xlsx file to make sure it
-### can fit in the R.
 ### PS: I really hate the data structure of OECD Database, though its visualization
 ### is not bad.
-### OECD database uses ".." to denote NA value.
-df_xlsx <- readxl::read_excel("./oecdecondata1.xlsx",range="A5:M923",na="..")
-str(df_xlsx)
-df_xlsx <- df_xlsx[,c(-2,-4)] # to delete the NA columns
-dim(df_xlsx)
+### Due to the merged cells in the .xlsx file, we use openxlsx::read.xlsx to deal with it.
+# install.packages("openxlsx")  To check if the package has been installed
+df_xlsx <- openxlsx::read.xlsx("./oecdecondata1.xlsx",rows=c(5:923),cols=c(1:13),fillMergedCells = T, na.strings="..")
+# Options: rows and columns specify the range of data we load into R
+# fillMergedCells=T specify we need to fill the merged cells with the data.
+# OECD database uses ".." to denote NA value. So we specify it in na.strings
+df_xlsx <- df_xlsx[,-4] # delete the fourth column due to it is na
+df_xlsx_2003 <- subset(df_xlsx,df_xlsx$Time==2003)
+oecd_countrylist <- c(df_xlsx_2003[1:36,1]) # to get the names of OECD countries
+# print(oecd_countrylist)
+for (i in c(1:nrow(df_xlsx))){
+  if (df_xlsx[i,1] %in% oecd_countrylist){
+    df_xlsx[i,1] <- "OECD Economies"
+  }
+} # The for loop above labels all the OECD economies
+vec_colnames <- c("Category","Country","Time",
+                  "Current account balance as a percentage of GDP",
+                  "Real effective exchange rate, constant trade weights",
+                  "Ratio of potential to actual real GDP of the total economy",
+                  "Consumer price index",
+                  "Consumer price index, harmonised",
+                  "Gross domestic product, market prices, deflator",
+                  "Labour productivity, total economy",
+                  "Unemployment rate",
+                  "Long-term interest rate on government bonds")
+names(df_xlsx) <- vec_colnames # Rename the variables
+str(df_xlsx) # Check again the structure of the data.frame
 
 ## Delim .txt
 df_delim <- read.delim("./oecdecondata.txt")
@@ -43,8 +63,8 @@ str(df_delim)
 # Checking imports
 str(df_bes_dta)
 str(df_bes_sav)
-head(df_bes_dta,5)
-head(df_bes_sav,5)
+head(df_bes_dta)
+head(df_bes_sav)
 ## The column names of dta dataframe and csv dataframe are slightly different.
 ## The column names in csv dataframe are all in upper case.
 ## But in the dta dataframe, they are all in lower case.
